7e4ce45454eb88967a8b8b04ef57dbae64ceb1d0
Javier Samper <jsamperarias@hotmail.com>
2018-12-10 15:56:51 +0100

    Login,signup,logout

--- server/app.js
+++ server/app.js
@@ -3,15 +3,14 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors = require('cors');
 
 const session    = require("express-session");
 const MongoStore = require('connect-mongo')(session);
-const flash      = require("connect-flash");
+
     
 
 mongoose
@@ -34,36 +33,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
-
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
-hbs.registerHelper('ifUndefined', (value, options) => {
-  if (arguments.length < 2)
-      throw new Error("Handlebars Helper ifUndefined needs 1 parameter");
-  if (typeof value !== undefined ) {
-      return options.inverse(this);
-  } else {
-      return options.fn(this);
-  }
-});
-  
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
 // Enable authentication using session + passport
 app.use(session({
   secret: 'irongenerator',
@@ -71,8 +40,13 @@ app.use(session({
   saveUninitialized: true,
   store: new MongoStore( { mongooseConnection: mongoose.connection })
 }))
-app.use(flash());
 require('./passport')(app);
+
+
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000'],
+}));
     
 
 const index = require('./routes/index');
@@ -80,6 +54,10 @@ app.use('/', index);
 
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
-      
+
+
+app.use((req, res) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
 
 module.exports = app;
