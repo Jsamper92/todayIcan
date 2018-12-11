eb319f3146f97a461070d04168a5016aa3152e8e
Jsamper92 <jsamperarias@hotmail.com>
2018-12-11 18:07:36 +0100

    Revert "Javier"

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,62 +1,39 @@
 const express = require("express");
 const passport = require('passport');
-const authRoutes = express.Router();
+const router = express.Router();
 const User = require("../models/User");
-const uploadCload = require("../config/cloudinary");
 
 // Bcrypt to encrypt passwords
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 
 
-authRoutes.post("/login", function (req, res, next) {
-  passport.authenticate('local', function (err, user, info) {
-    
-    if (err) {
-      return res.status(500).json({
-        message: "Error login"
-      })
-    }
-    if (!user) {
-      return res.status(500).json({
-        message: "Error login"
-      })
-    }
-
-    req.logIn(user, function (err) {
-      if (err) {
-        return res.status(500).json({
-          message: "Error login"
-        })
-      }
-      return res.status(200).json(user);
-    });
-  })(req, res, next);
+router.get("/login", (req, res, next) => {
+  res.render("auth/login", { "message": req.flash("error") });
 });
 
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/auth/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
 
-authRoutes.post("/signup", uploadCload.single("photo"), (req, res, next) => {
-  const {
-    username,
-    password
-  } = req.body;
-  const pictureUrl = req.body.url;
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
 
-
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
   if (username === "" || password === "") {
-    res.status(500).json({
-      message: "Indicate username and password"
-    });
+    res.render("auth/signup", { message: "Indicate username and password" });
     return;
   }
 
-  User.findOne({
-    username
-  }, "username", (err, user) => {
+  User.findOne({ username }, "username", (err, user) => {
     if (user !== null) {
-      res.status(500).json({
-        message: "The username already exists"
-      })
+      res.render("auth/signup", { message: "The username already exists" });
       return;
     }
 
@@ -65,47 +42,22 @@ authRoutes.post("/signup", uploadCload.single("photo"), (req, res, next) => {
 
     const newUser = new User({
       username,
-      password: hashPass,
-      pictureUrl
+      password: hashPass
     });
 
-    newUser.save((err, user) => {
-      if (err) {
-        res.status(500).json({
-          message: "Something went wrong"
-        });
-      } else {
-        req.login(user, (err) => {
-
-          if (err) {
-            res.status(500).json({
-              message: 'Login after signup went bad.'
-            });
-            return;
-          }
-
-          res.status(200).json(user);
-        });
-      }
-    });
+    newUser.save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch(err => {
+      res.render("auth/signup", { message: "Something went wrong" });
+    })
   });
 });
 
-authRoutes.get("/logout", (req, res) => {
+router.get("/logout", (req, res) => {
   req.logout();
-  res.status(200).json({
-    message: "Logout"
-  });
+  res.redirect("/");
 });
 
-authRoutes.get('/loggedin', (req, res) => {
-  if (req.isAuthenticated()) {
-    return res.status(200).json(req.user);
-  } else {
-    return res.status(403).json({
-      message: "Unauthorized"
-    });
-  }
-})
-
-module.exports = authRoutes;
+module.exports = router;
